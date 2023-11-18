e8e302b20bdd779fd3136753452eadb069154f11
vikassharma011 <67600288+vikassharma011@users.noreply.github.com>
2023-11-19 02:35:07 +0530

    Update dbConnect.js

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-//const URL = "mongodb://vikas:isa123@ac-wl0tzxg-shard-00-00.klmfzp0.mongodb.net:27017,ac-wl0tzxg-shard-00-01.klmfzp0.mongodb.net:27017,ac-wl0tzxg-shard-00-02.klmfzp0.mongodb.net:27017/?ssl=true&replicaSet=atlas-xcd22q-shard-0&authSource=admin&retryWrites=true&w=majority"
-dotenv.config();
+
 const dbConnect = async()=> {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
